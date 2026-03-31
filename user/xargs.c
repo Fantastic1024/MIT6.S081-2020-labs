ec9e19576257fc86ee4953bf998108e59fa6ae5c
fanluojun <fanluojun@qq.com>
2026-03-31 09:29:15 +0000

    重构 xargs 程序，提取读取行的功能到单独的 readOneLine 函数

--- user/xargs.c
+++ user/xargs.c
@@ -3,22 +3,40 @@
 #include "user/user.h"
 
 int
-main(int argc, char *argv[]){
-	int n;
-	int len = 0;
-	char c;  // inputing a line from pipe
-	char buf[512];
-	
+readOneLine(char *buf){  // Reading a line from pipe
+	char c;
+	int i = 0;
+	memset(buf, 0, 512);
+
 	while(read(0, &c, 1) == 1){
 		if(c == '\n'){
-			if(fork() == 0){
-				
-			}else{
-
-				wait(0);
-				exit(0);
-			}
-			memset(buf, 0, sizeof(buf));
+			buf[i] = '\0';
+			return i;  // return the index of \0
+		}else{
+			buf[i] = c;
+			i++;
 		}
 	}
+	return i;
+}
+
+int
+main(int argc, char *argv[]){
+	char buf[512];
+	int i;
+	while(readOneLine(buf)){
+		if(fork() == 0){
+			char *args[32];
+			for(i = 1; i < argc; i++){
+				args[i - 1] = argv[i];
+			}
+			args[i - 1] = buf;
+			args[i] = 0;
+			exec(argv[1], args);
+			exit(1);
+		}else{
+			wait(0);
+		}
+	}
+	exit(0);
 }
